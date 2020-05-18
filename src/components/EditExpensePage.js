3c4bc526a09e5d4e9c6d97c82f5bd4a20d5cd77e
Winadi Wiratama <winadi.wiratama@dana.id>
2020-05-18 16:52:25 +0700

    #80 - organizing routes into separate file

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const EditExpensePage = () => (
+    <div>
+        <p>This is my edit expense component</p>
+    </div>
+);
+export default EditExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HelpPage = () => (
+    <div>
+        <p>This is my help page component!</p>
+    </div>
+);
+
+export default HelpPage;

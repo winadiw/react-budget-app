150f7b1e23508812281dd8dcdf8cb4cbd4509bb5
Winadi Wiratama <winadi.wiratama@dana.id>
2020-05-18 16:28:47 +0700

    #78 - 404 page handling

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {
   BrowserRouter,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 
 import 'normalize.css/normalize.css';
@@ -32,14 +33,21 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 

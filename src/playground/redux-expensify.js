dabc8af039ec68d6a1fb96a0183ee82172b29a42
Winadi <winadiw@gmail.com>
2020-05-19 20:18:33 +0700

    #92 - using combineReducers to combine multiple reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,18 +1,50 @@
-import {createStore, combineReducers } from "redux";
+import { createStore, combineReducers } from "redux";
 
-const demoState = {
-  expenses: [{
-    id: '23132',
-    description: "January Rent",
-    notes: 'This was the final payment for that address',
-    amount: 54500,
-    createdAt: 0
-  }],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount', // date or amount
-    startDate: undefined,
-    endDate: undefined
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
   }
 };
 
+// Filters Reducer
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined,
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+//Store creation
+const store = createStore(combineReducers({
+  expenses: expensesReducer,
+  filters: filtersReducer
+}));
+console.log(store.getState());
+
+const demoState = {
+  expenses: [
+    {
+      id: "23132",
+      description: "January Rent",
+      notes: "This was the final payment for that address",
+      amount: 54500,
+      createdAt: 0,
+    },
+  ],
+  filters: {
+    text: "rent",
+    sortBy: "amount", // date or amount
+    startDate: undefined,
+    endDate: undefined,
+  },
+};

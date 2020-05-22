fabb663ff3bff2620798d6c0ef1aeb84148a884f
Winadi Wiratama <winadi.wiratama@dana.id>
2020-05-22 18:29:52 +0700

    #120 - Snapshot testing with Dynamic Components

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ExpenseListItem = ({ id, description, amount, createdAt }) => {
+export const ExpenseListItem = ({ id, description, amount, createdAt }) => {
   return (
     <div>
       <Link to={`/edit/${id}`}>
---
+++ src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import expenses from "../fixtures/expenses";
+import { ExpenseListItem } from "../../components/ExpenseListItem";
+
+test('should render ExpenseListItem correctly', () => {
+    const expense = expenses[1];
+    const wrapper = shallow(<ExpenseListItem {...expense}/>)
+    expect(wrapper).toMatchSnapshot();
+})

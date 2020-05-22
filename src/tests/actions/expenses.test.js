93cecf93900bdd6b1ab53618f83397806f842a1e
Winadi Wiratama <winadi.wiratama@dana.id>
2020-05-22 14:52:29 +0700

    #116 - testing filters reducer

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,59 +1,51 @@
-import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
-test("should setup remove expense action object", () => {
-  const action = removeExpense({ id: "123abc" });
+test('should setup remove expense action object', () => {
+  const action = removeExpense({ id: '123abc' });
   expect(action).toEqual({
-    id: "123abc",
-    type: "REMOVE_EXPENSE",
+    type: 'REMOVE_EXPENSE',
+    id: '123abc'
   });
 });
 
-test('should setup edit expense', () => {
-  const updates = {
-    note: 'New note value'
-  };
-  const action = editExpense("123abc", updates);
-
+test('should setup edit expense action object', () => {
+  const action = editExpense('123abc', { notes: 'New note value' });
   expect(action).toEqual({
-    id: "123abc",
-    type: "EDIT_EXPENSE",
-    updates
+    type: 'EDIT_EXPENSE',
+    id: '123abc',
+    updates: {
+      notes: 'New note value'
+    }
   });
 });
 
-test("should setup add expense action object with provided values", () => {
+test('should setup add expense action object with provided values', () => {
   const expenseData = {
-    'description': "Rent",
+    description: 'Rent',
     amount: 109500,
     createdAt: 1000,
-    notes: "rent note"
-  }
-
+    notes: 'This was last months rent'
+  };
   const action = addExpense(expenseData);
-
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: {
       ...expenseData,
       id: expect.any(String)
     }
-  })
-})
+  });
+});
 
-test("should setup add expense action object with default values", () => {
-  const defaultValue = {
-    description: "",
-    notes: "",
-    amount: 0,
-    createdAt: 0,
-  };
-
+test('should setup add expense action object with default values', () => {
   const action = addExpense();
   expect(action).toEqual({
     type: 'ADD_EXPENSE',
     expense: {
-      ...defaultValue,
-      id: expect.any(String)
+      id: expect.any(String),
+      description: '',
+      notes: '',
+      amount: 0,
+      createdAt: 0
     }
-  })
-})
+  });
+});

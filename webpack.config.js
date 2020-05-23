6c8eb4a2ea1f7d2121b546f1b3afbecfc586bc3e
Winadi <winadiw@gmail.com>
2020-05-23 23:57:00 +0700

    #133 - Production webpack settings by passing env name, handle production devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
 // entry -> output
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js", //entry point for ReactDOM
-  output: {
-    //output dir to server
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        //to parse jsx, using babel loader
-        loader: "babel-loader",
-        test: /\.js$/, //only files that meet this criteria,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"), //to serve using webpack-dev-server,
-    historyApiFallback: true //for client side routing
-  },
+module.exports = (env) => {
+  const isProduction = env === "production";
+  return {
+    entry: "./src/app.js", //entry point for ReactDOM
+    output: {
+      //output dir to server
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          //to parse jsx, using babel loader
+          loader: "babel-loader",
+          test: /\.js$/, //only files that meet this criteria,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map", //handle super large files ize
+    devServer: {
+      contentBase: path.join(__dirname, "public"), //to serve using webpack-dev-server,
+      historyApiFallback: true, //for client side routing
+    },
+  };
 };

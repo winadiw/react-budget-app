e795dc9748f808ce5e382f5065c0867695681fbf
Winadi <winadiw@gmail.com>
2020-05-24 20:34:11 +0700

    #148 - CRUD in Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,16 +14,53 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
-database.ref().set({
-  name: "Winadi Wiratama",
-  age: 24,
-  isSingle: false,
-  location: {
-    city: "Jakarta",
-    country: "Indonesia",
-  },
-});
+database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+})
 
-// database.ref().set("This is my data")
-database.ref('age').set(27)
-database.ref('location/city').set("SLAKDla")
+// Setup data sub -> Andrew is a Software Developer at Amazon.
+
+// Change the data and make sure it reprints
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
+
+// database.ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   }).catch((e) => {
+//     console.log('Did not remove data', e);
+//   });

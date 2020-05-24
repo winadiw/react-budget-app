40584aa4f7bd2a295a49acaac99fa8cab320f309
Winadi <winadiw@gmail.com>
2020-05-24 20:50:22 +0700

    #150 - finished firebase tutorials

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,29 +14,55 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
-// Setup "expenses" with three items (description, note, amount, createdAt)
+// child_removed
+database.ref("expenses").on("child_removed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
 
-database.ref('expenses').push({
-  description: 'Rent',
-  note: '',
+// child_changed
+database.ref("expenses").on("child_changed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref("expenses").on("child_added", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+
+//     console.log(expenses);
+//   });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+
+//   console.log(expenses);
+// });
+
+database.ref("expenses").push({
+  description: "Rent",
+  note: "",
   amount: 109500,
-  createdAt: 976123498763
+  createdAt: 976123498763,
 });
-
-database.ref('expenses').push({
-  description: 'Phone bill',
-  note: '',
-  amount: 5900,
-  createdAt: 976123498763
-});
-
-database.ref('expenses').push({
-  description: 'Food',
-  note: '',
-  amount: 1200,
-  createdAt: 976123498763
-});
-
 
 // database.ref('notes/-Krll52aVDQ3X6dOtmS7').remove();
 

c866ecc6c3e0691cb579faa9a9c4e46f269e2a3b
Winadi <winadiw@gmail.com>
2020-05-25 13:45:41 +0700

    #159 - remove expense from firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,6 +7,7 @@ import {
   removeExpense,
   setExpenses,
   startSetExpenses,
+  startRemoveExpense,
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
@@ -35,6 +36,28 @@ test("should setup remove expense action object", () => {
     type: "REMOVE_EXPENSE",
     id: "123abc",
   });
+});
+
+test("should remove expense from firebase", (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+
+  store
+    .dispatch(startRemoveExpense({ id }))
+    .then(() => {
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual({
+        type: "REMOVE_EXPENSE",
+        id,
+      });
+
+      return database.ref(`expenses/${id}`).once("value");
+    })
+    .then((snapshot) => {
+      expect(snapshot.val()).toBeFalsy();
+      done();
+    });
 });
 
 test("should setup edit expense action object", () => {

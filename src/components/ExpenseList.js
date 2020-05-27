15741ee2b4c332209c07fc61f648e70923f3a863
Winadi <winadiw@gmail.com>
2020-05-27 20:28:28 +0700

    #178 - styling the list expenses finished

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,23 +5,29 @@ import selectExpenses from "../selectors/expenses";
 
 //export separately for testing purpose
 export const ExpenseList = (props) => (
-  <div>
-    <h1>Expense List</h1>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {props.expenses.length === 0 ? (
+        <div className="list-item list-item --message">
+          <span>No Expenses</span>
+        </div>
       ) : (
-          props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />;
-          })
-        )
-    }
+        props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />;
+        })
+      )}
+    </div>
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
-    expenses: selectExpenses(state.expenses, state.filters)
+    expenses: selectExpenses(state.expenses, state.filters),
   };
 };
 

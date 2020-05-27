ef88871af9051887f6ff930fbff8a3a071e4c0ed
Winadi <winadiw@gmail.com>
2020-05-27 18:31:56 +0700

    #172 - styling buttons for login, add some styles for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard" exact={true}>
+          <h1>Expensify</h1>
+        </Link>
         <button onClick={startLogout}>Logout</button>
-    </header>
+      </div>
+    </div>
+  </header>
 );
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        startLogout: () => dispatch(startLogout())
-    }
-}
+  return {
+    startLogout: () => dispatch(startLogout()),
+  };
+};
 export default connect(undefined, mapDispatchToProps)(Header);
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -10,9 +10,9 @@ export class LoginPage extends React.Component {
     return (
       <div className="box-layout">
         <div className="box-layout__box">
-          <h1 className="box-layout__title">Expensify App</h1>
+          <h1 className="box-layout__title">Expensify</h1>
           <p>It's time to get your expenses under control.</p>
-          <button onClick={this.onLogin}>Login</button>
+          <button onClick={this.onLogin} className="button">Login with Google</button>
         </div>
       </div>
     );

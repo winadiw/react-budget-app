60a1ce113062e5f380b11a4ff8e64975a704a275
Winadi <winadiw@gmail.com>
2020-05-27 18:44:24 +0700

    #173 - styling the summary area

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,27 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import numeral from "numeral";
 import getExpensesTotal from "../selectors/expenses-total";
 import getFilteredExpenses from "../selectors/expenses";
 
-export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
-  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? "expense" : "expenses";
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
 
   return (
-    <div>
-      <h1>Viewing {expenseCount} {expenseWord} totaling {formattedExpensesTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expenseCount}</span> {expenseWord} totaling{" "}
+          <span>{formattedExpensesTotal}</span>
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">
+            Add Expense
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   const visibleExpenses = getFilteredExpenses(state.expenses, state.filters);
 
   return {
     expenseCount: visibleExpenses.length,
-    expensesTotal: getExpensesTotal(visibleExpenses)
-  }
-}
+    expensesTotal: getExpensesTotal(visibleExpenses),
+  };
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);

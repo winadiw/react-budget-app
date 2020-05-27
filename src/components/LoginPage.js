48e0e44813205539dd6025961cdeaf88273abee5
Winadi <winadiw@gmail.com>
2020-05-27 18:17:45 +0700

    #171 - styling login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,24 +3,26 @@ import { startLogin } from "../actions/auth";
 import { connect } from "react-redux";
 
 export class LoginPage extends React.Component {
-
-    onLogin = () => {
-        this.props.startLogin()
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.onLogin}>Login</button>
-            </div>
-        );
-    }
+  onLogin = () => {
+    this.props.startLogin();
+  };
+  render() {
+    return (
+      <div className="box-layout">
+        <div className="box-layout__box">
+          <h1 className="box-layout__title">Expensify App</h1>
+          <p>It's time to get your expenses under control.</p>
+          <button onClick={this.onLogin}>Login</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        startLogin: () => dispatch(startLogin())
-    }
-}
+  return {
+    startLogin: () => dispatch(startLogin()),
+  };
+};
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
-
